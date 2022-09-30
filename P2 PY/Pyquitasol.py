ff371ccf6c3b0d65879e8836fdcb980c042035e2
505 <505@academicos.cesde.edu.co>
2022-09-30 16:43:10 -0500

    actualizacion taller

--- P2 PY/Pyquitasol.py
+++ P2 PY/Pyquitasol.py
@@ -8,11 +8,16 @@ tablasiembras = pd.read_csv('./BD Siembra/Siembras.csv')
 VeredaQuitasol = tablasiembras[(tablasiembras["Ciudad"]=="Bello")&(tablasiembras["Vereda"]=="Quitasol")]
 VeredaQuitasol = VeredaQuitasol.sort_values(by="Arboles")
 promedio = VeredaQuitasol.describe()
+tablaPromedio = promedio.loc[['mean']]
+arbolesPromedio = tablaPromedio['Arboles'].to_frame()
 
-
-archivoQuitasol=promedio.to_html()
-archivotexto1=open('VeredaQuitasol.html', "w",encoding="utf-8")
-archivotexto1.write(VeredaQuitasol)
-archivotexto1.close()
+'''archivoarbolesPromedio=arbolesPromedio.to_html()
+archivotexto1=open('arbolesPromedio.html', "w",encoding="utf-8")
+archivotexto1.write(archivoarboles)
+archivotexto1.close()'''
 
 ##como guardar el archivo para que genere el HTML con el promedio
+archivoVereda=VeredaQuitasol.to_html()
+archivotexto1=open('vereda.html', "w",encoding="utf-8")
+archivotexto1.write(archivoVereda)
+archivotexto1.close()
