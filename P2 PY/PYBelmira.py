171952332a07c8d9ae65e3687430106ae5c131a1
DaniMartinez <daniimartinez971@gmail.com>
2022-10-05 15:15:15 -0500

    actualizacion

--- P2 PY/PYBelmira.py
+++ P2 PY/PYBelmira.py
@@ -2,12 +2,18 @@ from statistics import median
 import pandas as pd
 
 tablasiembras = pd.read_csv('./BD Siembra/Siembras.csv')
+
 tablaBelmira = tablasiembras[(tablasiembras["Ciudad"]=="Belmira")]
 
+VeredasBelmira = tablaBelmira[(tablaBelmira["Vereda"]=="Rio Arriba")|(tablaBelmira["Vereda"]=="La Salazar")]
 
+
+<<<<<<< HEAD
+=======
 VeredasBelmira = tablaBelmira[(tablaBelmira["Vereda"]=="Rio Arriba")|(tablaBelmira=="La Salazar")]
 print(VeredasBelmira)
 
+>>>>>>> b935d3efd4012356e4a502d6b8b44b78fcd03bb4
 archivoVeredaBelmira=VeredasBelmira.to_html()
 archivotexto1=open('VeredasBelmira.html', "w",encoding="utf-8")
 archivotexto1.write(VeredasBelmira)
--- P2 PY/PYCaucasia.py
+++ P2 PY/PYCaucasia.py
@@ -4,11 +4,15 @@ import pandas as pd
 tablasiembras = pd.read_csv('./BD Siembra/Siembras.csv')
 
 HectareasCaucasia = tablasiembras[(tablasiembras["Ciudad"]=="Caucasia")&(tablasiembras["Hectareas"])]
-print(HectareasCaucasia)
+SoloHectarea = HectareasCaucasia["Hectareas"].to_frame()
 
-archivoHectareasMed=HectareasCaucasia.to_html()
-archivotexto1=open('HectareasMed.html', "w",encoding="utf-8")
-archivotexto1.write(HectareasMed)
+
+SoloHectarea=SoloHectarea.to_html()
+archivotexto1=open('SoloHectarea.html', "w",encoding="utf-8")
+archivotexto1.write(SoloHectarea)
 archivotexto1.close()
 
+<<<<<<< HEAD
+=======
 #Falta mostrar las hectareas
+>>>>>>> b935d3efd4012356e4a502d6b8b44b78fcd03bb4
